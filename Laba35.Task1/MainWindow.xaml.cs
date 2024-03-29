40deaa1537783777dce12545019466f915bfefcd
Yura buryy <buryy137@gmail.com>
2024-03-29 15:11:17 +0300

    add laba35

---
+++ Laba35.Task1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba35.Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button1.Visibility = Visibility.Collapsed;
+            AnimateWidthChange();
+        }
+
+        private void AnimateWidthChange()
+        {
+            Random random = new Random();
+            double newWidth = random.Next(200, 600); 
+            double currentWidth = ActualWidth;
+
+            // Создаем анимацию изменения ширины
+            DoubleAnimation animation = new DoubleAnimation(currentWidth, newWidth, TimeSpan.FromSeconds(1));
+
+            //animation.Completed += (s, e) =>
+            //{
+            //    // Когда анимация завершена, вызываем ее снова для создания эффекта бесконечной анимации
+            //    AnimateWidthChange();
+            //};
+
+            BeginAnimation(WidthProperty, animation);
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Button2.Visibility = Visibility.Collapsed;
+            AnimateWidthChange();
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Button3.Visibility = Visibility.Collapsed;
+            AnimateWidthChange();
+        }
+    }
+}
---
+++ Laba35.Task2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba35.Task2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AnimateMarginLeft();
+        }
+
+
+        public void AnimateMarginLeft()
+        {
+            Thickness currentMargin = Circle.Margin;
+            
+            double day = GetPercentageOfDayPassed();
+            
+            Thickness newMargin = new Thickness(800 * day, currentMargin.Top, currentMargin.Right, currentMargin.Bottom);
+
+            ThicknessAnimation animation = new ThicknessAnimation()
+            {
+                From = currentMargin,
+                To = newMargin,
+                Duration = TimeSpan.FromSeconds(1)
+            };
+
+            Circle.BeginAnimation(FrameworkElement.MarginProperty, animation);
+        }
+
+        public static double GetPercentageOfDayPassed()
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime startOfDay = currentTime.Date;
+            TimeSpan timePassed = currentTime - startOfDay;
+            double percentage = (timePassed.TotalSeconds / 86400);
+            return percentage;
+        }
+    }
+}
